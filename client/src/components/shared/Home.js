28cbed9cfcd3475e86ca9ec44c842b0d160691fe
Henry Doan <henrydoan96@gmail.com>
2019-07-07 14:38:04 -0600

    all auth set up with react set up

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const Home = () => (
+  <Header as="h3" textAlign="center">Img-hosting-react-rails</Header>
+)
+
+export default Home;
