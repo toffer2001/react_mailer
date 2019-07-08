e077d3ecad98f6f77aee31d5b9fd3eae0c2a14b6
Henry Doan <henrydoan96@gmail.com>
2019-07-08 17:33:22 -0600

    working

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -21,6 +21,7 @@ class Api::UsersController < ApplicationController
     if user.save
       render json: user
     else
+
       render json: { errors: user.errors.full_messages }, status: 422
     end
   end
